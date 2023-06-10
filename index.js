d574af7f4a2605b7d13e4e00ac9b469341a33644
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-10 15:37:06 +0530

    playSounds

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-//     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-//         // var audio=new Audio("sounds/tom-1.mp3");
-//         // audio.play();
-//         this.style.color = 'blue';
-//     });
-// }
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var key=this.innerHTML;
+        switch (key) {
+          case 'w':
+            new Audio('sounds/crash.mp3').play();
+            break;
 
-function BellBoy(name,age,hasWorkPermit,languages){  // Constructor Function -- start function name with Capital Letter
-    this.name=name;
-    this.age=age;
-    this.hasWorkPermit=hasWorkPermit;
-    this.languages=languages;
+          case 'a':
+            new Audio('sounds/tom-4.mp3').play();
+            break;
+
+          case 's':
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+
+          case 'd':
+            new Audio('sounds/snare.mp3').play();
+            break;
+
+          case 'j':
+            new Audio('sounds/tom-2.mp3').play();
+            break;
+
+          case 'k':
+            new Audio('sounds/tom-3.mp3').play();
+            break;
+
+          case 'l':
+            new Audio('sounds/kick-bass.mp3').play();
+            break;
+
+          default:
+            console.log(this);
+            break;
+        }   
+    });
 }
-
-var bellBoy01=new BellBoy("tim",21,true,["japanese","english"]);
-
-console.log(bellBoy01.age);
