f147b183afc9cb813ee52691e3d9add94fbd4b26
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-10 13:32:23 +0530

    play audion when clicked

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-//     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-//         alert("I got Clicked!");
-//     });
-// }
-
-function add(num1,num2){
-    return num1+num2;
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var audio=new Audio("sounds/tom-1.mp3");
+        audio.play();
+    });
 }
-
-function minus(num1,num2){
-    return num1-num2;
-}
-
-function multiply(num1,num2){
-    return num1*num2;
-}
-
-function devide(num1,num2){
-    return Number(num1/num2).toFixed(2);
-}
-
-function calculator(num1,num2,operator){
-    return operator(num1,num2);
-}
-
-
-console.log(calculator(8,3,add));
