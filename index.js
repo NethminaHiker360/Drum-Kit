4ee241cd8f931d5cbec1968aa4bcaac157e1e716
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-10 13:15:57 +0530

    float number

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        alert("I got Clicked!");
-    });
+// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+//         alert("I got Clicked!");
+//     });
+// }
+
+function add(num1,num2){
+    return num1+num2;
 }
+
+function minus(num1,num2){
+    return num1-num2;
+}
+
+function multiply(num1,num2){
+    return num1*num2;
+}
+
+function devide(num1,num2){
+    return Number(num1/num2).toFixed(2);
+}
+
