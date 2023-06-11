e5aa1102c917f65b4c9d4829acc0f2c5c1bbc1f4
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-11 10:27:28 +0530

    make sounds using js

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        var key=this.innerHTML;
-        switch (key) {
-          case 'w':
-            new Audio('sounds/crash.mp3').play();
-            break;
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    var key = this.innerHTML;
+    switch (key) {
+      case 'w':
+        new Audio('sounds/crash.mp3').play();
+        break;
 
-          case 'a':
-            new Audio('sounds/tom-4.mp3').play();
-            break;
+      case 'a':
+        new Audio('sounds/tom-4.mp3').play();
+        break;
 
-          case 's':
-            new Audio('sounds/tom-1.mp3').play();
-            break;
+      case 's':
+        new Audio('sounds/tom-1.mp3').play();
+        break;
 
-          case 'd':
-            new Audio('sounds/snare.mp3').play();
-            break;
+      case 'd':
+        new Audio('sounds/snare.mp3').play();
+        break;
 
-          case 'j':
-            new Audio('sounds/tom-2.mp3').play();
-            break;
+      case 'j':
+        new Audio('sounds/tom-2.mp3').play();
+        break;
 
-          case 'k':
-            new Audio('sounds/tom-3.mp3').play();
-            break;
+      case 'k':
+        new Audio('sounds/tom-3.mp3').play();
+        break;
 
-          case 'l':
-            new Audio('sounds/kick-bass.mp3').play();
-            break;
+      case 'l':
+        new Audio('sounds/kick-bass.mp3').play();
+        break;
 
-          default:
-            console.log(this);
-            break;
-        }   
-    });
+      default:
+        break;
+    }
+  });
 }
