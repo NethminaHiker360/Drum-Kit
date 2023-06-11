c790dfc7b2ec3c403db49102b956a688cc33a770
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-11 11:27:30 +0530

    play sound with key

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
     var key = this.innerHTML;
-    switch (key) {
-      case 'w':
-        new Audio('sounds/crash.mp3').play();
-        break;
-
-      case 'a':
-        new Audio('sounds/tom-4.mp3').play();
-        break;
-
-      case 's':
-        new Audio('sounds/tom-1.mp3').play();
-        break;
-
-      case 'd':
-        new Audio('sounds/snare.mp3').play();
-        break;
-
-      case 'j':
-        new Audio('sounds/tom-2.mp3').play();
-        break;
-
-      case 'k':
-        new Audio('sounds/tom-3.mp3').play();
-        break;
-
-      case 'l':
-        new Audio('sounds/kick-bass.mp3').play();
-        break;
-
-      default:
-        break;
-    }
+    playSound(key);
   });
+}
+
+document.addEventListener('keypress', function (event) {
+  playSound(event.key);
+});
+
+function playSound(key) {
+  switch (key) {
+    case 'w':
+      new Audio('sounds/crash.mp3').play();
+      break;
+
+    case 'a':
+      new Audio('sounds/tom-4.mp3').play();
+      break;
+
+    case 's':
+      new Audio('sounds/tom-1.mp3').play();
+      break;
+
+    case 'd':
+      new Audio('sounds/snare.mp3').play();
+      break;
+
+    case 'j':
+      new Audio('sounds/tom-2.mp3').play();
+      break;
+
+    case 'k':
+      new Audio('sounds/tom-3.mp3').play();
+      break;
+
+    case 'l':
+      new Audio('sounds/kick-bass.mp3').play();
+      break;
+
+    default:
+      break;
+  }
 }
